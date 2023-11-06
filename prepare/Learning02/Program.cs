25d8d87af29c4f507e3bda8ec6cc95312255748b
janrz03 <ruizgarcia.jr03@gmail.com>
2023-11-05 22:11:08 -0500

    W02 Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "DataBase Engineer";
+        job1._company = "Google";
+        job1._startYear = 2016;
+        job1._endYear = 2019;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "VMware";
+        job2._startYear = 2019;
+        job2._endYear = 2023;
+        //step 4 Test your job class 
+        //Console.WriteLine($"{job1._company}");
+        //Console.WriteLine($"{job2._company}");
+
+        //Step 5 Add a Display method 
+        //job1.Display(); 
+        //job2.Display(); 
+
+        Resume myResume = new Resume();
+        myResume._name = "Marcos Perez";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        //Step 7 Test your Resume class
+        //Console.WriteLine($"{myResume._jobs[0]._jobTitle}");
+
+        //Step 8 Add a Display method to the Resume class
+        myResume.Display();
     }
 }
